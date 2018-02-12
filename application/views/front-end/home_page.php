ffaa77523ddaf23eb1660c909f9c529b7adcaa7e
Blake Paige <itumelengseeletsa@gmail.com>
2018-02-12 20:24:50 +0200

    About us giving me problems

--- application/views/front-end/home_page.php
+++ application/views/front-end/home_page.php
@@ -1,5 +1,6 @@
 <?php  
 defined('BASEPATH') OR exit('No direct script access allowed');?>
+
 <div class="main-header">
 	<div class="background-banner-image">
 
@@ -16,5 +17,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
 		
 		</div>
 		
+	</div>
+</div>
+<div class="container">
+	<div class="about-us-section">
+		<h2>
+			About Us
+		</h2>
+		<hr class="about-us-underline">
+		<p>
+			The group was founded in response to an identified need of law firms to strategically position themselves for the future and access the economies of scale of a national group with national visibility and brand awareness.
+		</p>
 	</div>
 </div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/bootstrap.css") ?>">
-		<!--<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/font-awesome.min.css") ?>">-->
+		<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/fontawesome/on-server/css/fontawesome.css") ?>">
 		<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/styling.css") ?>">
 	</head>
 <body>
