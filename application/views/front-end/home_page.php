69a9508d17ee31419665e4bc0854096b26a5e438
Blake Paige <itumelengseeletsa@gmail.com>
2018-02-12 21:09:29 +0200

    Current blocks not working out

--- application/views/front-end/home_page.php
+++ application/views/front-end/home_page.php
@@ -19,14 +19,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
 		
 	</div>
 </div>
+	<div class="contain-about-us">
+		<div class="about-us-section">
+			<h2>
+				About Us
+			</h2>
+			<hr class="about-us-underline">
+			<p class="col-md-12 col-sm-12 col-xs-12">
+				The group was founded in response to an identified need of law firms to strategically position themselves for the future and access the economies of scale of a national group with national visibility and brand awareness.
+			</p>
+		</div>
+	</div>
 <div class="container">
-	<div class="about-us-section">
-		<h2>
-			About Us
-		</h2>
-		<hr class="about-us-underline">
-		<p>
-			The group was founded in response to an identified need of law firms to strategically position themselves for the future and access the economies of scale of a national group with national visibility and brand awareness.
-		</p>
+	<div class="row">
+		<!-- The First Thingggy -->
+		<div class="col-md-4 col-sm-4 col-xs-12 blocks-padding">
+			<div class="first-container">
+				Hello
+			</div>
+		</div>
+		<div class="col-md-8 col-sm-8 col-xs-12 blocks-padding">
+			<div class="first-text-containment">
+				
+			</div>
+		</div>
+		<br>
+		<!-- The Second Thingggy -->
+		<div class="col-md-8 col-sm-8 col-xs-12 blocks-padding">
+			<div class="second-text-containment">
+				
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-4 col-xs-12 blocks-padding">
+			<div class="second-container">
+				
+			</div>
+		</div>
+		<br>
+		<!-- The third Thingggy -->
+		<div class="col-md-4 col-sm-4 col-xs-12 blocks-padding">
+			<div class="third-container">
+				
+			</div>
+		</div>
+
+		<div class="col-md-8 col-sm-8 col-xs-12 blocks-padding">
+			<div class="third-text-containment">
+				
+			</div>
+		</div>
+		<br>
 	</div>
 </div>
